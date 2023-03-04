7245be680b152a7995b6e9de4685b5ef130be360
Donny Nguyen <donnynguyen631@gmail.com>
2023-03-03 23:06:48 -0500

    scraper

--- IA (2023)/scraper.java
+++ IA (2023)/scraper.java
@@ -4,7 +4,8 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
-
+import java.util.Map;
+import java.util.HashMap;
 
 public class scraper {
     public static void main(String[] args) throws IOException {
@@ -21,16 +22,66 @@ public class scraper {
         Document doc = Jsoup.connect("https://finance.yahoo.com/quote/" + stockName + "?p=" + stockName + "&.tsrc=fin-srch").get();
         
         //Gets the last fin-streamer element which contains the price of the selected stock
+        // Element price = doc.select("fin-streamer[data-field=regularMarketPrice]").last();
         Element price = doc.select("fin-streamer[data-field=regularMarketPrice]").last();
 
         //Gets the last fin-streamer element which contains the price of the selected stock
         Element title = doc.select("h1[class=D(ib) Fz(18px)]").first();
 
-        String scrapedPrice = log("Price: %s", price.text());
-        String scrapedTitle = log("Title: %s", title.text());
+        // String scrapedPrice = log("Price: %s", price.text());
+        // String scrapedTitle = log("Title: %s", title.text());
+
+        String scrapedPrice = price.text();
+        String scrapedTitle = title.text();
+
 
         return scrapedPrice + "\n" + scrapedTitle;
 
+    }
+
+    public Map<Integer, String> createHashMap(String stockName) {
+        Map<Integer, String> data = new HashMap<>();
+        
+        try {
+            Document doc = Jsoup.connect("https://finance.yahoo.com/quote/" + stockName + "/history?p=" + stockName).get();
+            // Elements row = doc.select("tr[class=BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)]");
+            // Elements row = doc.select("tr[class=BdT Bdc($seperatorColor) Ta(end) Fz(s) Whs(nw)]");
+
+            // Elements row = doc.select("td[class=Py(10px) Ta(start) Pend(10px)]");
+            // for(int i = 0; i < row.size(); i++) {
+            //     System.out.println(row.get(i).text());
+            // }
+
+            // Elements closePrice = doc.select("td[class=Py(10px) Pstart(10px)]");
+
+            // for(int i = 0; i < closePrice.size(); i++) {
+            //     System.out.println(closePrice.get(i).text());
+            // }
+
+            Element closePrice = doc.select("td[class=Py(10px) Pstart(10px)]");
+
+            System.out.println(closePrice.text());
+
+
+            // Element element = doc.selectFirst("td[class=Py(10px) Ta(start) Pend(10px)]");
+            // System.out.println("This is: " + element);
+            // if (element != null) {
+            //     System.out.println(element);
+            // }     
+
+            // Elements col = row.select("td[class=Ta(end) Fw(600) Lh(14px)]");
+            // Elements col2 = row.select("td[class=Ta(end) Fw(600) Lh(14px) Pend(10px)]");
+
+
+
+            data.put(1, "Apple");
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Error" + e);
+        }
+        return data;
+        
     }
 
     private static String log(String msg, String... vals) {
