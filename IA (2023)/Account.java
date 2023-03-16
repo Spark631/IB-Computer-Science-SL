7fdb2adc56fb7ca5d8cb0047862b7d36b8135d42
Donny Nguyen <donnynguyen631@gmail.com>
2023-03-15 23:51:21 -0400

    Fixed sell method

--- IA (2023)/Account.java
+++ IA (2023)/Account.java
@@ -4,6 +4,15 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.OutputKeys;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import javax.xml.transform.TransformerException;
+import java.io.OutputStream;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -26,6 +35,7 @@ public class Account {
 
             // root element
             Element rootElement = doc.createElement("Account");
+            // rootElement.setAttribute("xml:space", "preserve");
             doc.appendChild(rootElement);
 
             // user element
@@ -61,7 +71,6 @@ public class Account {
             transformer.transform(source, consoleResult);
 
         } catch (Exception e) {
-            // TODO: handle exception
             System.out.println("Error" + e);
         }
     }
@@ -115,30 +124,21 @@ public class Account {
 
             LinkedList<Stock> stockLists = new LinkedList<Stock>();
 
-            Stock stock = new Stock("","",0,0.0,0.0,0.0,0.0,"");
-
             for (int i = 0; i < stockList.getLength(); i++) {
                 Element stocks = (Element) stockList.item(i);
                 String name = stocks.getElementsByTagName("name").item(0).getTextContent();
                 String ticker = stocks.getElementsByTagName("ticker").item(0).getTextContent();
-                int amountOfShares = Integer.parseInt(stocks.getElementsByTagName("amountOfShares").item(0).getTextContent());
-                Double stockPrice = Double.parseDouble(stocks.getElementsByTagName("stockPrice").item(0).getTextContent());
+                int amountOfShares = Integer
+                        .parseInt(stocks.getElementsByTagName("amountOfShares").item(0).getTextContent());
+                Double stockPrice = Double
+                        .parseDouble(stocks.getElementsByTagName("stockPrice").item(0).getTextContent());
                 Double net = Double.parseDouble(stocks.getElementsByTagName("net").item(0).getTextContent());
                 Double total = Double.parseDouble(stocks.getElementsByTagName("total").item(0).getTextContent());
-                Double totalGained = Double.parseDouble(stocks.getElementsByTagName("totalGained").item(0).getTextContent());
+                Double totalGained = Double
+                        .parseDouble(stocks.getElementsByTagName("totalGained").item(0).getTextContent());
                 String sector = stocks.getElementsByTagName("sector").item(0).getTextContent();
 
-                // System.out.println("Name: " + name);
-                // System.out.println("Ticker: " + ticker);
-                // System.out.println("Amount of Shares: " + amountOfShares);
-                // System.out.println("Stock Price: " + stockPrice);
-                // System.out.println("Net: " + net);
-                // System.out.println("Total: " + total);
-                // System.out.println("Total Gained: " + totalGained);
-                // System.out.println("Sector: " + sector);
-                // System.out.println("\n");
-
-                stock = new Stock(name,ticker,amountOfShares,stockPrice,net,total,totalGained,sector);
+                Stock stock = new Stock(name, ticker, amountOfShares, stockPrice, net, total, totalGained, sector);
                 stockLists.add(stock);
             }
             return stockLists;
@@ -202,6 +202,7 @@ public class Account {
 
     public void updateBalance(double money) {
         try {
+            System.out.println("THIS IS THE LIKE OMG LIKE WORKING LIKE" + money);
             File inputFile = new File("account.xml");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
@@ -211,15 +212,19 @@ public class Account {
             NodeList userList = doc.getElementsByTagName("user");
             Element user = (Element) userList.item(0);
             double balance = Double.parseDouble(user.getElementsByTagName("wallet").item(0).getTextContent());
-            balance = Math.round((balance - money) * 100.00) / 100.00;
-            System.out.println("BALANCEEEEEE: " + balance);
+            balance = Math.round((balance + money) * 100.00) / 100.00;
+            System.out.println("Your new balance should be: " + balance);
+            System.out.println("this is your balance: " + user.getElementsByTagName("wallet").item(0).getTextContent()
+                    + "This is your money: " + money);
             user.getElementsByTagName("wallet").item(0).setTextContent(Double.toString(balance));
-
+            System.out.println("GHUDFGHJKDFGHJDFJKGJKFHDGJKHDFJKGDFG"
+                    + user.getElementsByTagName("wallet").item(0).getTextContent());
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(doc);
             StreamResult result = new StreamResult(new File("account.xml"));
             transformer.transform(source, result);
+            System.out.println(source);
 
         } catch (Exception e) {
             System.out.println("Error: " + e);
@@ -233,11 +238,15 @@ public class Account {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
+            // doc.getDocumentElement().normalize();
 
+            Element user = (Element) doc.getElementsByTagName("user").item(0);
+            user.setAttribute("xml:space", "preserve");
+
+            // Get the existing <Account> element
             Element account = (Element) doc.getElementsByTagName("Account").item(0);
 
-            // Create a new Stock element
+            // Create a new <Stock> element
             Element stock = doc.createElement("Stock");
 
             // Create child elements for the stock information
@@ -265,6 +274,10 @@ public class Account {
             total.appendChild(doc.createTextNode(Double.toString(stockInfo.getTotal())));
             stock.appendChild(total);
 
+            Element totalSpent = doc.createElement("totalSpent");
+            totalSpent.appendChild(doc.createTextNode(Double.toString(stockInfo.getTotalMoneySpent())));
+            stock.appendChild(totalSpent);
+
             Element totalGained = doc.createElement("totalGained");
             totalGained.appendChild(doc.createTextNode(Double.toString(stockInfo.getTotalMoneyGained())));
             stock.appendChild(totalGained);
@@ -273,21 +286,106 @@ public class Account {
             sector.appendChild(doc.createTextNode(stockInfo.getSector()));
             stock.appendChild(sector);
 
-            // Add the <Stock> element to the <Account> element
+            // Append the new <Stock> element to the existing <Account> element
+            account.appendChild(doc.createTextNode("\n"));
             account.appendChild(stock);
 
+            // Set the indent and indent amount properties on the transformer
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
-            transformer.setOutputProperty(javax.xml.transform.OutputKeys.INDENT, "yes");
-            transformer.setOutputProperty(javax.xml.transform.OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+
+            // Write the transformed document to the input file
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File("account.xml"));
+            StreamResult result = new StreamResult(inputFile);
             transformer.transform(source, result);
+            // preserveElement(doc);
 
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
     }
+
+    public void sellStock(String stock, int shares) {
+        try {
+            File inputFile = new File("account.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            double total = 0;
+
+            NodeList stockList = doc.getElementsByTagName("Stock");
+            for (int i = 0; i < stockList.getLength(); i++) {
+                Element stockElement = (Element) stockList.item(i);
+                if (stockElement.getElementsByTagName("ticker").item(0).getTextContent().equals(stock.toUpperCase())) {
+                    int amountOfShares = Integer
+                            .parseInt(stockElement.getElementsByTagName("amountOfShares").item(0).getTextContent());
+                    if (amountOfShares >= shares) {
+                        amountOfShares -= shares;
+                        stockElement.getElementsByTagName("amountOfShares").item(0)
+                                .setTextContent(Integer.toString(amountOfShares));
+                        System.out.println("You have sold " + shares + " shares of " + stock);
+
+                        Scraper scraper = new Scraper();
+                        double price = scraper.findStockPrice(stock);
+                        total = price * shares;
+
+                        System.out.println("You have gained " + price * shares + " dollars");
+                        break;
+                    } else {
+                        System.out.println("You do not have enough shares to sell");
+                    }
+                }
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(inputFile);
+            transformer.transform(source, result);
+
+            updateBalance(total);
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    // public static void preserveElement(Document doc) {
+    // Nodelist stockNodes = doc.getElementsByTagName("Stock");
+    // for (int i = 0; i < stockNodes.getLength(); i++) {
+    // Element stock = (Element) stockNodes.item(i);
+    // stock.setAttribute("xml:space", "preserve");
+    // }
+    // TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    // Transformer transformer = transformerFactory.newTransformer();
+    // transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+    // transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount",
+    // "4");
+    // DOMSource source = new DOMSource(doc);
+    // StreamResult result = new StreamResult(new File("account.xml"));
+    // transformer.transform(source, result);
+
+    // }
+
+    // private static void writeXml(Document doc,
+    // OutputStream output)
+    // throws TransformerException {
+
+    // TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    // Transformer transformer = transformerFactory.newTransformer();
+
+    // // pretty print
+    // transformer.setOutputProperty(javax.xml.transform.OutputKeys.INDENT, "yes");
+
+    // DOMSource source = new DOMSource(doc);
+    // StreamResult result = new StreamResult(output);
+
+    // transformer.transform(source, result);
+
+    // }
 
 }
